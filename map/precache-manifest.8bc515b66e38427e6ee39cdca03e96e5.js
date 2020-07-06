85d0111c95c1607ec2ad1e1b477381a87edaae3f
Athishay <athishayk@gmail.com>
2020-07-06 10:18:54 +0530

    Added map view

---
+++ map/precache-manifest.8bc515b66e38427e6ee39cdca03e96e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd404477f0a5d3a319b186058356ed58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6442ad4a8848895cac5e",
+    "url": "/static/css/main.5727f4e9.chunk.css"
+  },
+  {
+    "revision": "558049b880ccfa74c502",
+    "url": "/static/js/2.d87c134a.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/static/js/2.d87c134a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6442ad4a8848895cac5e",
+    "url": "/static/js/main.cf55b658.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
